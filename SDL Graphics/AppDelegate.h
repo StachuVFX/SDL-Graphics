aa459f08b9aef9b2bdc57241680d3f3850931362
Stanisław Trela <stachuvfx@icloud.com>
2025-10-26 11:15:05 +0100

    Initial Commit

---
+++ SDL Graphics/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SDL Graphics
+//
+//  Created by Stachu on 26.10.2025.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
